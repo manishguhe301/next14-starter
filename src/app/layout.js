855c8e1c2dd769d841c110574de0ee4785078109
Manish Guhe <manishguhe301@gmail.com>
2024-05-22 20:10:10 +0530

    added favicon and page titlle

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import Footer from '@/components/footer/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
+  title: 'MG',
+  description: 'Creative Thoughts Agency.',
 };
 
 export default function RootLayout({ children }) {
